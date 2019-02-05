b6dbcf79dc8fb7c4b5fa8561ece0137017edd583
digitallyamar <digitallyamar@gmail.com>
2019-02-05 11:11:22 +0530

    A simple Python 3 Pelican static website generator installer script to get Pelican up and running on a Linux machine. Prerequisite: pip3 and Python 3 installation

---
+++ Python3_Pelican_Installer.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+pip3 install pelican markdown
+
+
+mkdir Pelican_Demo
+cd Pelican_Demo
+
+pelican-quickstart
+
+echo "Title: My First Review" >> content/keyboard-review.md
+echo "Date: 2019-02-05 05:10" >> content/keyboard-review.md
+echo "Category: Review" >> content/keyboard-review.md
+echo "\n" >> content/keyboard-review.md
+echo "Title: My First Review using Pelican" >> content/keyboard-review.md
+
+pelican content
+
+#Open newly created html content in your default browser
+echo "Your website is now LIVE at http://localhost:8000/"
+
+pelican --listen
